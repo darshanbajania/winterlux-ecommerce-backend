f6c579ff81556a45cee1d958cb6447cdd4a5c46f
darshanbajania <darshanbajania1999@gmail.com>
2025-12-06 21:33:45 +0530

    feat: Add Vercel frontend URL to CORS allowed origins

--- ecommerce_backend/settings.py
+++ ecommerce_backend/settings.py
@@ -69,6 +69,7 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://winterlux-ecommerce.vercel.app"
 ]
 
 ROOT_URLCONF = 'ecommerce_backend.urls'
